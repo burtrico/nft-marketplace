3ae134c2529bcfae9f6fd822f43b040e2a87e99a
Joe Renteria <joe@electronflight.com>
2021-08-24 14:15:48 -0500

    api working

--- src/NftApi.js
+++ src/NftApi.js
@@ -1,22 +1,23 @@
-import react , {useState , useEffect} from "react"
+import react , {useState , useEffect} from "react";
 
 function NftApi(){
 
     const [data,setData] = useState([]);
 
     useEffect(function(){
-        fetch("https://api.opensea.io/api/v1/collections?offset=0&limit=300")
+        fetch("https://api.opensea.io/api/v1/collections?offset=0&limit=100")
         .then(response => response.json())
         .then(response => setData(response));
       },[])
-
-
     return(<>
+
+
+   
         {
             data.collections.map(
                 function(objectIn){
                       return(
-                          <span>{objectIn.image_url}</span>
+                          <img src={objectIn.image_url}/>
                           
                       )
                 }
