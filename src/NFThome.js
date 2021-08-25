12690d5711af5c9f5712a868920692684352ab42
Michael Burt <burtrico@gmail.com>
2021-08-24 20:22:42 -0500

    burt 4

--- src/NFThome.js
+++ src/NFThome.js
@@ -2,24 +2,24 @@ import {useState , useEffect} from "react";
 import NFTlist from "./NFTlist";
 import NFTwallet from "./NFTwallet";
 
-const NFTs = [
-        {
-            id: 1,
-            name: "beef",
-            price: 5.99,
-            section: "meat",
-            description: "Delicious cow meat, moo moo!",
-            image: "https://www.kroger.com/product/images/medium/front/0001111096970"
-        },
-        {
-            id: 2,
-            name: "chicken",
-            price: 3.99,
-            section: "meat",
-            description: "Delicious chicken meat, bock bock!",
-            image: "https://www.kroger.com/product/images/medium/front/0002410558105"
-        }
-  ]
+// const NFTs = [
+//         {
+//             id: 1,
+//             name: "beef",
+//             price: 5.99,
+//             section: "meat",
+//             description: "Delicious cow meat, moo moo!",
+//             image: "https://www.kroger.com/product/images/medium/front/0001111096970"
+//         },
+//         {
+//             id: 2,
+//             name: "chicken",
+//             price: 3.99,
+//             section: "meat",
+//             description: "Delicious chicken meat, bock bock!",
+//             image: "https://www.kroger.com/product/images/medium/front/0002410558105"
+//         }
+//   ]
 
 function NFThome(){
 
@@ -37,7 +37,7 @@ function NFThome(){
         fetch("https://api.opensea.io/api/v1/collections?offset=0&limit=100")
         .then(response => response.json())
         .then(response => 
-            setData(response)
+            setData(response.collection)
             
         );
         setLoading(false)
@@ -49,16 +49,14 @@ function NFThome(){
     return(
         <>
         { console.log(walletNFTs) }
+       
         <NFTlist data={data} 
         addToWallet={addToWallet} />
 
         <NFTwallet walletNFTs={walletNFTs}
         addToWallet={addToWallet}
          />
-
-
-   
-
+        
         </>
         )
   
