badb67e7b71fde8640d971666c2aae764540db27
Michael Burt <burtrico@gmail.com>
2021-08-27 14:36:59 -0500

    done

--- src/NFTwhat.js
+++ src/NFTwhat.js
@@ -30,15 +30,33 @@ function NFTwhat(props) {
                       
                     >
                     <div>
-                      <div class="headingwhat"><img src={What} alt="What is an NFT?"/></div><br></br>
-                      
-                      An NFT is a unit of data stored on a digital ledger, called a blockchain, which can be sold and traded. The NFT can be associated with a particular digital or physical asset (such as a file or a physical object) and a license to use the asset for a specified purpose. NFTs (and the associated license to use, copy or display the underlying asset) can be traded and sold on digital markets.
-
-NFTs function like cryptographic tokens, but, unlike cryptocurrencies like Bitcoin, NFTs are not mutually interchangeable, so not fungible. While all bitcoins are equal, each NFT may represent a different underlying asset and thus have a different value. NFTs are created when blockchains string records of cryptographic hash, a set of characters identifying a set of data, onto previous records therefore creating
- a chain of identifiable data blocks. This cryptographic transaction process ensures the authentication of each digital file by providing a digital signature that is used to track NFT ownership. However, data links that point to details like where the art is stored can die.</div>
-
-                      
-  
+                      <div class="headingwhat"><img src={What} alt="What is an NFT?"/></div>
+                      <br>
+                      </br>
+                      <ul>
+                        <li className="nftWhat">
+                        An NFT is a unit of data stored on a digital ledger, called a blockchain, which can be sold and traded.
+                        </li>
+                        <li className="nftWhat">
+                        The NFT can be associated with a particular digital or physical asset (such as a file or a physical object) and a license to use the asset for a specified purpose.
+                        </li>
+                        <li className="nftWhat">
+                        NFTs (and the associated license to use, copy or display the underlying asset) can be traded and sold on digital markets.
+                        </li>
+                        <li className="nftWhat">
+                        NFTs function like cryptographic tokens, but, unlike cryptocurrencies like Bitcoin, NFTs are not mutually interchangeable, so not fungible.
+                        </li>
+                        <li className="nftWhat">
+                        While all bitcoins are equal, each NFT may represent a different underlying asset and thus have a different value. 
+                        </li>
+                        <li className="nftWhat">
+                        NFTs are created (minted) when blockchains string records of cryptographic hash, a set of characters identifying a set of data, onto previous records therefore creating a chain of identifiable data blocks. 
+                        </li>
+                        <li className="nftWhat">
+                        This cryptographic transaction process ensures the authentication of each digital file by providing a digital signature that is used to track NFT ownership. However, data links that point to details like where the art is stored can die.
+                        </li>
+                      </ul>
+                      </div>
                     </Grid>
 
 
